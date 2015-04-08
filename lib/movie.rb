5f219ebb7dc8fa63d11901fb0a233f83153b4612
Eder Roger Souza <eder.roger.souza@gmail.com>
2015-04-07 21:37:53 -0300

    add more classes and polimorphic behavior to calculate prices

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,12 +1,32 @@
+require 'new_release_price'
+require 'regular_price'
+require 'childrens_price'
+
 class Movie
   REGULAR = 0
   NEW_RELEASE = 1
   CHILDRENS = 2
 
-  attr_reader :title
-  attr_accessor :price_code
+  attr_reader :title, :price_code
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
+  end
+
+  def price_code=(value)
+    @price_code = value
+    @price = case price_code
+             when REGULAR then RegularPrice.new
+             when NEW_RELEASE then NewReleasePrice.new
+             when CHILDRENS then ChildrensPrice.new
+             end
+  end
+
+  def charge(days_rented)
+    @price.charge(days_rented)
+  end
+
+  def frequent_renter_points(days_rented)
+    (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
   end
 end
